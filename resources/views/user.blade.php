1825219de6424cbec677a379ffce76f6cf1be3c1
Leo <leo.guedes@aluno.ce.gov.br>
2024-05-24 00:39:18 -0300

    Read e etc

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,9 +4,12 @@
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         Users
     </h2>
+    <ul>
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach 
+    </ul>
+
 
 @endsection
-
-
-
 
